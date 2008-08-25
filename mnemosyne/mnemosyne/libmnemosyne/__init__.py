45e42f5f1c6931db494c36a586d55b7a0223083a
pbienst <pbienst@e5e6b78b-db40-0410-9517-b98c64f8d2c1>
2008-08-25 19:19:35 +0000

    More config work.
    git-svn-id: https://mnemosyne-proj.svn.sourceforge.net/svnroot/mnemosyne-proj/trunk@539 e5e6b78b-db40-0410-9517-b98c64f8d2c1

--- mnemosyne/mnemosyne/libmnemosyne/__init__.py
+++ mnemosyne/mnemosyne/libmnemosyne/__init__.py
@@ -15,7 +15,7 @@ import os
 import sys
 
 import mnemosyne.version
-from mnemosyne.libmnemosyne.component_manager import component_manager
+from mnemosyne.libmnemosyne.component_manager import component_manager, config
 from mnemosyne.libmnemosyne.exceptions import * # TODO: remove
 
 log = logging.getLogger("mnemosyne")
@@ -83,14 +83,12 @@ def initialise_system_components():
     # Database.
     from mnemosyne.libmnemosyne.databases.pickle import Pickle
     component_manager.register("database", Pickle())
+    
     # Scheduler.
     from mnemosyne.libmnemosyne.schedulers.SM2_mnemosyne \
                                                    import SM2Mnemosyne
     component_manager.register("scheduler", SM2Mnemosyne())
-    # Fact filters.
-    from mnemosyne.libmnemosyne.filters.escape_to_html \
-                                                   import EscapeToHtml
-    component_manager.register("fact_filter", EscapeToHtml())
+    
     # Card types.
     from mnemosyne.libmnemosyne.card_types.front_to_back import FrontToBack
     component_manager.register("card_type", FrontToBack())
@@ -98,9 +96,16 @@ def initialise_system_components():
     component_manager.register("card_type", BothWays())
     from mnemosyne.libmnemosyne.card_types.three_sided import ThreeSided
     component_manager.register("card_type", ThreeSided())
-    # Card Filters.
-    from mnemosyne.libmnemosyne.filters.make_html import MakeHtml
-    component_manager.register("card_filter", MakeHtml())
+    
+    # Renderer.
+    from mnemosyne.libmnemosyne.renderers.html_css import HtmlCss
+    component_manager.register("renderer", HtmlCss())
+    
+    # Filters.
+    from mnemosyne.libmnemosyne.filters.escape_to_html \
+                                                   import EscapeToHtml
+    component_manager.register("filter", EscapeToHtml())
+    
     # File formats.
 
     # Function hooks.
