bafcc8113fc8d29b7188c06440ad38a4bec9568c
Ed Bartosh <ed@esdhcp034217.research.nokia.com>
2008-10-21 19:38:56 +0000

    fixed lintian bugs, added copyright, etc

--- mnemosyne/mnemosyne/pomni/scripts/xdxfconv.py
+++ mnemosyne/mnemosyne/pomni/scripts/xdxfconv.py
@@ -1,6 +1,30 @@
 #!/usr/bin/python -tt
 #vim: sw=4 ts=4 expandtab ai
 
+""" Converter from XDXF(http://xdxf.sourceforge.net/) 
+    to Mnemosyne (http://www.mnemosyne-proj.org) database format 
+"""
+
+__copyright__ = """
+Copyright (C) 2008 Ed Bartosh <bartosh@gmail.com>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+of the Software, and to permit persons to whom the Software is furnished to do
+so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.  THE SOFTWARE IS PROVIDED "AS
+IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
+LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE
+AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+"""
+
 import sys
 import os
 import xml.sax
@@ -8,38 +32,52 @@ import xml.sax
 from optparse import OptionParser
 from xml.sax.handler import ContentHandler
 
-from mnemosyne.libmnemosyne.card_types.three_sided import ThreeSided
-from mnemosyne.libmnemosyne.component_manager import ui_controller_main, database, config, card_types
 from mnemosyne import libmnemosyne
+from mnemosyne.libmnemosyne.card_types.three_sided import ThreeSided
+from mnemosyne.libmnemosyne.component_manager import database, config, \
+                                              ui_controller_main
 
 class XDXFHandler(ContentHandler):
     """ XDXF format handler """
 
     def __init__(self, output):
-        self.tagnames = ('k', 'tr', 'ar')
+
+        ContentHandler.__init__(self)
+
+        self.tagnames = ("k", "tr", "ar")
         self.state = dict(zip(self.tagnames, [False] * len(self.tagnames)))
-        self.key = self.transcription = ''
+        self.key = self.transcription = ""
         self.translation = []
         self.output = output
 
     def startElement(self, name, attrs):
+        """ callback for the start of an element """
         self.state[name] = True
 
     def endElement(self, name):
+        """ callback for the end of an element """
+        
         self.state[name] = False
-        if name == 'ar':
+        if name == "ar":
             self.output.out(self)
             self.translation = []
 
     def characters(self, content):
-        if self.state['k']:
-            self.key = content.encode('utf8').strip()
-        elif self.state['tr']:
-            self.transcription = content.encode('utf8').strip()
-        elif self.state['ar']:
-            self.translation.extend([c.encode('utf8').strip() \
-                for c in content.split("\n") \
-                    if c and c.encode('utf8').strip() != self.key])
+        """ callback for character data """
+
+        if self.state["k"]:
+            #self.key = content.encode("utf8").strip()
+            self.key = content
+        elif self.state["tr"]:
+            self.transcription = content
+            #self.transcription = content.encode("utf8").strip()
+        elif self.state["ar"]:
+            self.translation.extend([c for c in content.split("\n") 
+                                        if c != self.key])
+
+            #self.translation.extend([c.encode("utf8").strip() \
+            #    for c in content.split("\n") \
+            #        if c and c.encode("utf8").strip() != self.key])
 
 class TextOut(object):
     """ Text output """
@@ -48,25 +86,30 @@ class TextOut(object):
         self.fptr = fptr
     
     def out(self, obj):
+        """ main entry point. Called from XDXF parser """
+
         if obj.transcription:
-            self.fptr.write("%s\t[%s]\n%s" % (obj.key, obj.transcription, "\n".join(obj.translation)))
+            self.fptr.write("%s\t[%s]\n%s" % (obj.key, obj.transcription, 
+                            "\n".join(obj.translation)))
         else:
             self.fptr.write("%s\t%s" % (obj.key, "\n".join(obj.translation)))
 
-class PickleOut(object):
+class MnemosyneOut(object):
+    """ Output to Mnemosyne Db """
     
-    def __init__(self, dbname="default.mem", datadir=None, category=None, records=None):
+    def __init__(self, datadir=None, category=None, records=None):
         
         if datadir:
             datadir = os.path.abspath(datadir)
         elif os.path.exists(os.path.join(os.getcwdu(), ".mnemosyne")):
             datadir = os.path.abspath(os.path.join(os.getcwdu(), ".mnemosyne"))
         else:
-            datadir = os.path.abspath(os.path.join(os.path.expanduser('~'), ".mnemosyne"))
+            datadir = os.path.abspath(os.path.join(os.path.expanduser("~"), 
+                        ".mnemosyne"))
 
         libmnemosyne.initialise(datadir)
+
         self.card_type = ThreeSided()
-        self.dbname = dbname
         self.database = database()
         self.saved = False
         
@@ -76,79 +119,88 @@ class PickleOut(object):
             self.records = -1
 
         if not category:
-            self.category = ['category1']
+            self.category = ["category1"]
         else:
             self.category = [category]
 
         self.controller = ui_controller_main()
 
     def out(self, obj):
-        """ Insert into the database """
+        """ main entry point. Called from XDXF parser """
+
         def fact_exists(name):
+            """ Check if fact with the same key is 
+                in database
+            """
             for fact in self.database.facts:
-                if fact.data['f'] == name:
+                if fact.data["f"] == name:
                     return True
-        
-        if self.records and len(obj.key) > 3 and not fact_exists(obj.key): # skip duplicate names and short words
-            fact = {'f': obj.key, 't': obj.translation}
-            if obj.transcription:
-                fact['p'] = obj.transcription
-            else:
-                fact['p'] = ''
        
-            self.controller.create_new_cards(fact, self.card_type, 0, [self.category])
+        # skip duplicate names and short words (ugly)
+        if self.records and len(obj.key) > 3 and not fact_exists(obj.key):
+            fact = {"f": obj.key, "t": obj.translation}
+            if obj.transcription:
+                fact["p"] = obj.transcription
+            else:
+                fact["p"] = ""
+       
+            self.controller.create_new_cards(fact, self.card_type, 0, 
+                                            [self.category])
             self.records -= 1
 
         if not self.records:
             self.savedb()
 
     def savedb(self):
+        """ Save the database if not saved yet """
+
         if not self.saved:
-            self.database.save(config()['path'])
+            self.database.save(config()["path"])
             self.saved = True
 
     def __del__(self):
         """ Save the database on exit """
+
         self.savedb()
+        libmnemosyne.finalise()
 
 def parse_commandline(argv):
     """ Parse commandline, check options """
     
-    parser = OptionParser(usage = "%prog [options] <xdxf file> [<output file>]")
+    parser = OptionParser(usage = "%prog [options] <xdxf file>")
 
-    parser.add_option("-c", "--category", help="specify category name", metavar="catname")
-    parser.add_option("-f", "--format", type="choice", choices=("pickle", "text"), default="pickle", metavar="format", \
-        help="output format: pickle or text, [default: %default]")
+    parser.add_option("-c", "--category", help="specify category name", 
+        metavar="category")
+    parser.add_option("-f", "--format", type="choice", 
+        choices=("mnemosyne", "text"), default="mnemosyne", metavar="format",
+        help="output format: mnemosyne or text, [default: %default]")
     parser.add_option("-d", "--datadir", help="data directory")
-    parser.add_option("-r", "--records", type="int", help="amount of records to load")
+    parser.add_option("-r", "--records", type="int", 
+        help="amount of records to load")
 
     options, argv = parser.parse_args(argv)
 
     if len(argv) < 2:
         parser.error("missed mandatory parameter <xdxf file>")
+    if not os.access(argv[1], os.R_OK):
+        parser.error("file %s isn't readable or doesn't exist" % argv[1])
 
     return (options, argv)
 
 def main(argv):
+    """ Main """
 
     opts, argv = parse_commandline(argv)
 
     parser = xml.sax.make_parser()
 
-    outfn = None
-    if len(argv) > 2:
-        outfn = argv[2]
-
-    if not opts.format or opts.format == 'pickle':
-        out = PickleOut(outfn, opts.datadir, opts.category, opts.records)
+    if not opts.format or opts.format == "mnemosyne":
+        out = MnemosyneOut(opts.datadir, opts.category, opts.records)
     else:
-        outfp = None 
-        if outfn:
-            outfp = open(outfn, 'w')
-        out = TextOut(outfp)
+        out = TextOut()
 
     parser.setContentHandler(XDXFHandler(out))
-    parser.parse(open(argv[1], 'r'))
+    parser.parse(open(argv[1], "r"))
     return 0
 
 if __name__ == "__main__":
