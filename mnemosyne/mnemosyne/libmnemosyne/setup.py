84481d39cd47aa7fe214a07480e56792083e84d7
Ed Bartosh <bartosh@gmail.com>
2008-11-29 17:51:56 +0000

    created package for libmnemosyne

---
+++ mnemosyne/mnemosyne/libmnemosyne/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(name = "libmnemosyne",
+    description='Mnemosyne core library',
+    version = 2.0,
+    author = "Peter Bienstman",
+    author_email = "Peter.Bienstman@UGent.be",
+    license='GPL 2',
+    packages = [ "mnemosyne.libmnemosyne"] + ["mnemosyne.libmnemosyne.%s" % pkg \
+	for pkg in ["card_types", "databases", "file_formats", "filters", "loggers", 
+	"renderers", "schedulers", "ui_controllers_main", "ui_controllers_review"]
+    ],
+    package_dir = {'mnemosyne.libmnemosyne': '', 'mnemosyne': 'debian'},
+    classifiers = [
+    'Development Status :: 3 - Alpha',
+    'Environment :: Console',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: GNU General Public License (GPL)',
+    'Natural Language :: English',
+    'Operating System :: POSIX :: Linux',
+    'Programming Language :: Python',
+    'Topic :: Software Development :: Libraries',
+    ]
+)
+
