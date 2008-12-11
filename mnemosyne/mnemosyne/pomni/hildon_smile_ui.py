8a76d75fbfe20e276a22408e4cfb75e7af07583b
Vlad Vasiliev <vlad@vladbook.oblgaz>
2008-12-11 23:31:19 +0200

    Finished eternal disign of smile theme

--- mnemosyne/mnemosyne/pomni/hildon_smile_ui.py
+++ mnemosyne/mnemosyne/pomni/hildon_smile_ui.py
@@ -21,7 +21,7 @@
 #
 
 """
-Programming of "eternal" design 
+Programming of "draft" design 
 """
 
 from pomni.hildon_ui import HildonUiControllerReview
@@ -33,32 +33,35 @@ class HildonThemeUiControllerReview(HildonUiControllerReview):
         """ Initialization items of review window for Eternal theme """
 
         HildonUiControllerReview.__init__(self)
-        self.get_answer_hbox = None
         self.estimate_box = None
         self.answer_box = None
-        self.answer_box = None
-        self.w_tree = None
+        self.eventbox_show_answer = None
 
     def show_answer(self):
         """ Show answer in review window for eternal theme """
 
-        self.get_answer_hbox.set_property('visible', False)
-        self.estimate_box.set_property('visible', True)
-        self.answer_box.set_property('visible', True)
         self.answer.set_text(self.card.answer())
+        self.eventbox_numeral0.set_sensitive(True)
+        self.eventbox_numeral1.set_sensitive(True)
+        self.eventbox_numeral2.set_sensitive(True)
+        self.eventbox_numeral3.set_sensitive(True)
+        self.eventbox_numeral4.set_sensitive(True)
+        self.eventbox_numeral5.set_sensitive(True)
+        self.eventbox_show_answer.set_sensitive(False)
 
     def start(self, w_tree):
         """ Start new review window for eternal theme """
 
         HildonUiControllerReview.start(self, w_tree)
-        self.get_answer_hbox = self.w_tree.get_widget("get_answer_hbox")
+        self.eventbox_show_answer = \
+             self.w_tree.get_widget("eventbox_show_answer")
+        #Connect to various signals
+        self.w_tree.signal_autoconnect(
+          {"on_eventbox_show_answer_button_press_event": 
+           self.open_card_clicked})
+
         self.estimate_box = self.w_tree.get_widget("estimate_box")
         self.answer_box = self.w_tree.get_widget("answer_box")
-
-        #Connect to various signals
-        self.w_tree.signal_autoconnect({
-           "on_eventbox_show_answer_button_press_event": self.open_card_clicked
-        })
 
         #Begin the review window from a new question
         self.new_question()
@@ -66,10 +69,13 @@ class HildonThemeUiControllerReview(HildonUiControllerReview):
 
     def theme_new_question(self):
         """ Visible and Unvisible some items of review windows """
-        self.estimate_box.set_property('visible', False)
-        self.answer_box.set_property('visible', False)
-        self.get_answer_hbox.set_property('visible', True)
-
+        self.eventbox_numeral0.set_sensitive(False)
+        self.eventbox_numeral1.set_sensitive(False)
+        self.eventbox_numeral2.set_sensitive(False)
+        self.eventbox_numeral3.set_sensitive(False)
+        self.eventbox_numeral4.set_sensitive(False)
+        self.eventbox_numeral5.set_sensitive(False)
+        self.eventbox_show_answer.set_sensitive(True)
 
 def _test():
     """ Run doctests
