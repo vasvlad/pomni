60efae977a5c21d5845f680d7b67ca9b7c29474d
Ed Bartosh <bartosh@gmail.com>
2008-12-24 20:40:43 +0000

    first draft of sqlite backend. mnemosyne can be loaded wiht this code

--- mnemosyne/mnemosyne/libmnemosyne/databases/sqlite.py
+++ mnemosyne/mnemosyne/libmnemosyne/databases/sqlite.py
@@ -5,8 +5,12 @@
 # Author: Ed Bartosh <bartosh@gmail.com>
 #
 
-from sqlite3 import connect
+from datetime import datetime
+from sqlite3 import connect, OperationalError
 
+from mnemosyne.libmnemosyne.start_date import StartDate
+from mnemosyne.libmnemosyne.utils import expand_path, contract_path
+from mnemosyne.libmnemosyne.component_manager import config, log
 from mnemosyne.libmnemosyne.database import Database
 
 class Sqlite(Database):
@@ -17,75 +21,152 @@ class Sqlite(Database):
 
     def __init__(self):
         self.connection = None
-        self.cursor = None
+        self.path = None
+        self.start_date = None
+        self.load_failed = False
+        self.start_date = None
 
     def new(self, path):
+        """ Create new database """
+        
+        if self.is_loaded():
+            self.unload()
+
+        self.start_date=StartDate()
+        config()["path"] = path
+        log().new_database()
+
         connection = connect(path)
-        cursor = connection.cursor()
         
-        cursor.execute('''create table facts
-        (id integer primary key, guid int, facttype_id int, 
-        ctime timestamp, mtime timestamp)''')
+        connection.executescript('''
+            create table facts(
+                id integer primary key, 
+                guid int, 
+                facttype_id int, 
+                ctime timestamp, 
+                mtime timestamp
+            );
 
-        cursor.execute('''create table factdata
-        (id integer primary key, fact_id int, key text, value text)''')
+            create table factdata(
+                id integer primary key,
+                fact_id int,
+                key text,
+                value text
+            );
 
-        cursor.execute('''create table facttypes
-        (id integer primary key, name varchar(20), enabled boolean)''')
+            create table facttypes(
+                id integer primary key,
+                name varchar(20) UNIQUE NOT NULL,
+                enabled default TRUE
+            );
 
-        cursor.execute('''create table views
-        (id integer primary key, facttype_id int, name varchar(20),
-        enabled boolean)''')
+            create table views(
+                id integer primary key,
+                facttype_id int,
+                name varchar(20) UNIQUE NOT NULL,
+                enabled boolean default TRUE
+            );
 
-        cursor.execute('''create table reviewstats
-        (id integer primary key, fact_id int, view_id int, grade int,
-        easiness int, next_rep timestamp)''')
-        
-        cursor.execute('''create table categories
-        (id integer primary key, parent_id int, name varchar(20), 
-        enabled boolean)''')
+            create table reviewstats(
+                id integer primary key,
+                fact_id int, 
+                view_id int, 
+                grade int,
+                easiness int, 
+                next_rep timestamp
+            );
 
-        cursor.execute('''create table fact_categories
-        (id integer primary key, category_id int, fact_id int)''')
+            create table categories(
+                id integer primary key, 
+                parent_id int default 0, 
+                name varchar(20) UNIQUE NOT NULL, 
+                enabled boolean default TRUE
+            );
 
-        cursor.execute('''create table reviews
-        (card_id int, time timestamp, number int, prev int, 
-        client_guid int)''')
+            create table fact_categories(
+                id integer primary key,
+                category_id int,
+                fact_id int
+            ); 
+            
+            create table reviews(
+                card_id int, 
+                time timestamp, 
+                number int, 
+                prev int, 
+                client_guid int
+            );
 
-        cursor.execute('''create table meta
-        (row_id int, key text, value text)''')
+            create table meta(
+                id integer primary key, 
+                key, 
+                value
+            );
+        ''')
 
-        self.cursor = cursor
+        # save start_date as a string. Object StartDate can't be stored
+        connection.execute("insert into meta(key, value) values(?,?)", 
+        ('start_date', datetime.strftime(self.start_date.start, 
+                                         '%Y-%m-%d %H:%M:%S')))
+
         self.connection = connection
+        self.save()
 
     def save(self):
-        raise NotImplementedError
+        """ Commit changes  """
+        if self.connection:
+            self.connection.commit()
 
     def backup(self):
         raise NotImplementedError
 
-    def load(self):
-        raise NotImplementedError
+    def load(self, fname):
+        """ Load database from file """
+        
+        # Unload opened database if exists
+        self.unload()
 
+        path = expand_path(fname, config().basedir)
+        try:
+            self.connection = connect(path)
+            res = self.connection.execute('select value from meta where key=?',
+                ('start_date',)).fetchone()
+            self.load_failed = False
+            self.path = path
+            self.set_start_date(StartDate(datetime.strptime(res[0], 
+                '%Y-%m-%d %H:%M:%S')))
+            self.load_failed = False
+            self.path = path
+        except OperationalError, exobj:
+            self.load_failed = True
+ 
     def unload(self):
-        raise NotImplementedError
+        """ Unload/Close the database """
 
-    def is_loaded():
-        raise NotImplementedError
+        self.save()
+        if self.connection:
+            self.connection.close()
+            self.connection = None
+            self.load_failed = False
+
+    def is_loaded(self):
+        """ Is it alive? """
+
+        return self.connection != None
 
     # Start date.
 
     def set_start_date(self, start_date_obj):
-        raise NotImplementedError
+        self.start_date = start_date_obj
 
     def days_since_start(self):
-        raise NotImplementedError
+        return self.start_date.days_since_start()
 
     # Adding, modifying and deleting categories, facts and cards.
 
     def add_category(self, category):
         raise NotImplementedError
-
+    
     def modify_category(self, modified_category):
         raise NotImplementedError
 
@@ -124,8 +205,13 @@ class Sqlite(Database):
         
     # Queries.
 
+    def category_names(self):
+        return self.connection.execute("select name from categories")
+
     def has_fact_with_data(self, fact_data):
-        raise NotImplementedError
+        return bool(self.connection.execute('''select count() from factdata
+            where key=? and value=?;''', 
+            (fact_data['q'], fact_data['a'])).fetchone()[0])
 
     def duplicates_for_fact(self, fact):
 
@@ -137,16 +223,26 @@ class Sqlite(Database):
         raise NotImplementedError
 
     def card_count(self):
-        raise NotImplementedError
+        return self.connection.execute("select count() from reviews").\
+            fetchone()[0]
 
     def non_memorised_count(self):
-        raise NotImplementedError
+        return self.connection.execute("""select count() from reviewstats
+            where grade < 2""").fetchone()[0]
 
-    def scheduled_count(self):
-        raise NotImplementedError
+    def scheduled_count(self, days=0):
+        """ Number of cards scheduled within 'days' days."""
+
+        return self.connection.execute("""select count() from reviewstats
+            where grade >=2 and ? >= next_rep - ?""", 
+            (self.days_since_start(), days)).fetchone()[0]
 
     def active_count(self):
-        raise NotImplementedError
+        """ Return number of cards in an active category """
+
+        return self.connection.execute("""select count() from fact_categories,
+                categories where category_id = categories.id and
+                categories.enabled=1""").fetchone()[0]
 
     def average_easiness(self):
         raise NotImplementedError
