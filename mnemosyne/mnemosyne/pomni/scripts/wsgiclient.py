4f3f31d96ba431c171f079540adbfa158b0db281
Ed Bartosh <bartosh@gmail.com>
2009-05-23 22:39:57 +0300

    WSGI: working incremental processing of POST!

--- mnemosyne/mnemosyne/pomni/scripts/wsgiclient.py
+++ mnemosyne/mnemosyne/pomni/scripts/wsgiclient.py
@@ -1,12 +1,85 @@
-import time, urllib
+import os, sys, random, time, urllib, httplib, urlparse
+import simplejson
 
 def parse_JSON(fileobj):
-   import simplejson, time
-   for data in fileobj:
+   for data in os.fdopen(fileobj.fileno(), 'rb', -1):
        simplejson.loads(data)
+
+def parse_XML(fileobj): # Incrementally.
+    from xml.etree.cElementTree import iterparse
+    context = iterparse(fileobj, events=("start", "end"))
+    root = None
+    for event, elem in context:
+        #print elem
+        if event == "start" and root is None:
+            root = elem     # The first element is root
+        if event == "end" and elem.tag == "record":
+          #... process record elements ...
+          root.clear()
+
+def randstr(length):
+    return ''.join([random.choice('abcdefghijklmnopqrstuvwxyz') \
+           for i in xrange(length)])
+
+def generate_JSON(cards=10000):
+    for cardid in xrange(cards):
+        data = [cardid, randstr(40), cardid, cardid,
+            random.randint(1,5), str(2.5),random.randint(1,100), str(0), str(0), str(10),
+            str(0.0), str(0.0), str(1), randstr(40), str(10), str(1), str(1), str(1),
+            randstr(40), randstr(40)]
+        yield "%s\n" % simplejson.dumps(data)
+
+def parse_uri(uri):
+    scheme, netplace, path, query, fragid = urlparse.urlsplit(uri)
+
+    if ':' in netplace: 
+        host, port = netplace.split(':', 2)
+        port = int(port)
+    else: 
+        host, port = netplace, 80
+
+    if query: 
+        path += '?' + query
+
+    return host, port, path
+
+def putdata(uri, iterator):
+    host, port, path = parse_uri(uri)
+    conn = httplib.HTTPConnection(host, port)
+    conn.putrequest('POST', path)
+    conn.putheader('User-Agent', 'mnemosyne/2.0')
+    conn.putheader('Connection', 'keep-alive')
+    conn.putheader('Content-Type', 'text/plain')
+    conn.putheader('Transfer-Encoding', 'chunked')
+    conn.putheader('Expect', '100-continue')
+    conn.putheader('Accept', '*/*')
+    conn.endheaders()
+
+    for chunk in iterator:
+        length = len(chunk)
+        conn.send('%X\r\n' % length)
+        conn.send(chunk + '\r\n')
+    conn.send('0\r\n\r\n')
+
+    resp = conn.getresponse()
+    body = resp.read()
+    body = body.rstrip('\r\n')
+    body = body.encode('string_escape')
+
+    return (resp.status, body) # an int
 
 if __name__ == "__main__":
     t1 = time.time()
-    parse_JSON(urllib.urlopen("http://192.168.255.17:9999/10000"))
+    #opener = urllib.FancyURLopener()
+    #opener.addheader('User-Agent', 'gzip')
+    #opener.addheader('Accept-Encoding', 'gzip')
+    #opener.addheaders = [('Accept-encoding', 'gzip'), ('User-Agent', 'gzip')] 
+    #urllib2.install_opener(opener)
+    #parse_XML(opener.open("http://mnemosyneweb.appspot.com/XML"))
+    #parse_JSON(opener.open("http://mnemosyneweb.appspot.com/JSON"))
+
+    #parse_JSON(urllib.urlopen("http://localhost:8080/JSON"), data)
+    #print putdata("http://localhost:8080/JSON", generate_JSON(3))
+    print putdata("http://localhost:9999", generate_JSON(10000))
     print time.time()-t1
 
--- mnemosyne/mnemosyne/pomni/scripts/wsgiserver.py
+++ mnemosyne/mnemosyne/pomni/scripts/wsgiserver.py
@@ -12,9 +12,9 @@ def generate_JSON(cards=10000):
             random.randint(1,5), str(2.5),random.randint(1,100), str(0), str(0), str(10),
             str(0.0), str(0.0), str(1), randstr(40), str(10), str(1), str(1), str(1),
             randstr(40), randstr(40)]
-        yield "%s\n" % simplejson.dumps(data)
+        yield "%s\n" % str(data)
 
-def wsgi_application(environ, start_response):
+def wsgi_application1(environ, start_response):
     cards = int(environ['PATH_INFO'].split('/')[1])
     print "%s asked for %d cards" % (environ['REMOTE_ADDR'], cards)
     start_response('200 OK', [('Content-Type', 'text/json')])
@@ -26,6 +26,55 @@ def wsgi_application(environ, start_response):
     print time.time()-t1
     print length
 
-httpd = make_server('', 9999, wsgi_application)
+from wsgiref.simple_server import ServerHandler, WSGIRequestHandler
+
+class MyServerHandler(ServerHandler):
+    def run(self, application):
+        """Invoke the application"""
+        try:
+            self.setup_environ()
+            self.result = application(self.environ, self.start_response, 
+                                        self.request_handler.rfile)
+            self.finish_response()
+        except:
+            try:
+                self.handle_error()
+            except:
+                # If we get an error handling an error, just give up already!
+                self.close()
+                raise   # ...and let the actual server figure it out.
+ 
+
+class MyWSGIRequestHandler(WSGIRequestHandler):
+    def handle(self):
+        self.raw_requestline = self.rfile.readline()
+        if not self.parse_request(): # An error code has been sent, just exit
+            return
+
+        handler = MyServerHandler(
+            self.rfile, self.wfile, self.get_stderr(), self.get_environ()
+        )
+        
+        handler.request_handler = self      # backpointer for logging
+        handler.run(self.server.get_app())
+
+def wsgi_application(environ, start_response, socket):
+    import simplejson
+    if environ['HTTP_TRANSFER_ENCODING'] == 'chunked':
+        i = 0
+        while True:
+            size = socket.readline()
+            if size == '0\r\n':
+                break
+            simplejson.loads(socket.readline())
+            i += 1
+            if socket.readline() != '\r\n':
+                start_response('400 Bad Request', [('Content-Type', 'text/html')])
+                return ["Wrong format of HTTP chunk\n\r"]
+
+    start_response('200 OK', [('Content-Type', 'text/json')])
+    return ["%d\n\r" % i]
+
+httpd = make_server('', 9999, wsgi_application, handler_class=MyWSGIRequestHandler)
 httpd.serve_forever()
 
