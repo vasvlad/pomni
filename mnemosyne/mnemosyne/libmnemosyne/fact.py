48fea4149b822fbcaa78464e071c158aec79b94b
umax <maxusachev@gmail.com>
2009-07-08 21:46:01 +0300

    test.

--- mnemosyne/mnemosyne/libmnemosyne/fact.py
+++ mnemosyne/mnemosyne/libmnemosyne/fact.py
@@ -35,6 +35,7 @@ class Fact(object):
     """
 
     def __init__(self, data, card_type, creation_time=None, id=None):
+        print "Fact.py __init__ method"
         if creation_time is None:
             creation_time = time.time()
         self.creation_time = creation_time
@@ -46,6 +47,19 @@ class Fact(object):
             id = str(uuid.uuid4())
         self.id = id
         self._id = None
+        print "fact.data =", self.data
+        print "fact.card_type =", self.card_type
+        print "--- card type ---"
+        print "fact.card_type.firelds =", self.card_type.fields
+        print "fact.card_type.required_firelds =", self.card_type.required_fields()
+        print "fact.card_type.keys() =", self.card_type.keys()
+        print "fact.card_type.key_names() =", self.card_type.key_names()
+        print "--- card type ---"
+        print "fact.id =", self.id
+        print "fact._id =", self._id
+        print "fact.creation_time =", self.creation_time
+        print "fact.modification_time =", self.modification_time
+        print "fact.instance =", self
 
     def __eq__(self, other):
         try:
