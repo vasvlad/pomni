d3e54f50df8e2e4f33c904ebe81ce69b1a0b66c5
umax <maxusachev@gmail.com>
2009-07-21 12:44:43 +0300

    modified sync script for server

--- mnemosyne/mnemosyne/pomni/sync.py
+++ mnemosyne/mnemosyne/pomni/sync.py
@@ -1,9 +1,12 @@
 import sys
+import os
 sys.path.insert(0, '../../')
+sys.path.insert(0, "../")
 from libSM2sync.server import Server
 from libSM2sync.server import WSGI
 from libSM2sync.client import Client
 from libSM2sync.client import HttpTransport
+from pomni.factory import app_factory
 
 def main(argv):
     """Main."""
@@ -14,9 +17,13 @@ def main(argv):
         mode = argv[1]
         uri = argv[2]
         if mode == "server":
+            app = app_factory()
+            app.initialise(os.path.abspath(os.path.join(os.getcwdu(), ".pomni")))
+            database = app.database()
             transport = WSGI(uri)
-            server = Server(transport, "database")
+            server = Server(transport, database)
             server.start()
+            app.finalise()
         elif mode == "client":
             transport = HttpTransport(uri)
             client = Client(transport, "database")
