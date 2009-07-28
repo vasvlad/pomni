57906efa63c651f3de747e6662e82471a7cfc88c
umax <maxusachev@gmail.com>
2009-07-28 17:26:32 +0300

    use urllib2 instead of httplib

--- mnemosyne/libSM2sync/client.py
+++ mnemosyne/libSM2sync/client.py
@@ -3,15 +3,19 @@ Client.
 """
 
 import mnemosyne.version
-import httplib
 import base64
 import urllib2
 import uuid
 import os
-from urlparse import urlparse
 from sync import SyncError
 from sync import EventManager
 from sync import PROTOCOL_VERSION, N_SIDED_CARD_TYPE
+
+
+#Overrides get_method method for using PUT request in urllib2
+class PutRequest(urllib2.Request):
+    def get_method(self):
+        return "PUT"
 
 
 class Client:
@@ -22,9 +26,6 @@ class Client:
         self.config = config
         self.database = database
         self.uri = uri
-        params = urlparse(uri)
-        self.host = params.scheme
-        self.port = params.path
         self.backup_file = None
         self.eman = EventManager(database, controller)
         self.id = hex(uuid.getnode())
@@ -45,7 +46,7 @@ class Client:
             self.backup_file = self.database.make_sync_backup()
             #self.eman.apply_history(server_history)
             #client_history = self.eman.get_history()
-            #self.send_history(client_history)
+            #self.send_client_history(client_history)
         except SyncError, exception:
             #FIXME: replace by ErrorDialog
             print exception
@@ -59,44 +60,33 @@ class Client:
         base64string = base64.encodestring("%s:%s" % (self.config['user_id'], \
             self.config['user_passwd']))[:-1]
         authheader =  "Basic %s" % base64string
-        #FIXME: it necessary for localhost
-        if not self.uri.startswith("http://"):
-            uri = "http://" + self.uri
-        else:
-            uri = self.uri
-        request = urllib2.Request(uri)
+        request = urllib2.Request(self.uri)
         request.add_header("AUTHORIZATION", authheader)
         try:
             urllib2.urlopen(request)
-        except urllib2.URLError, exception:
-            if hasattr(exception, 'code'):
-                if exception.code == 403:
+        except urllib2.URLError, error:
+            if hasattr(error, 'code'):
+                if error.code == 403:
                     raise SyncError(\
                         "Authentification failed: wrong login or password!")
-                else:
-                    raise SyncError(exception)
             else:
-                raise SyncError(exception.reason)
+                raise SyncError(str(error.reason))
 
     def handshake(self):
         """Handshaking with server."""
 
+        cparams = "<params><client id='%s' name='%s' ver='%s' protocol='%s'" \
+            " deck='%s' cardtypes='%s' extra='%s'/></params>\n" % (self.id, \
+            self.name, self.version, self.protocol, self.deck, self.cardtypes, \
+            self.extra)
         try:
-            conn = httplib.HTTPConnection(self.host, self.port)
-            conn.request('GET', '/sync/server/params')
-            server_params = conn.getresponse().read()
-            client_params = "<params><client id='%s' name='%s' ver='%s'" \
-                " protocol='%s' deck='%s' cardtypes='%s' extra='%s'/>" \
-                " </params>\n" % (self.id, self.name, self.version, \
-                self.protocol, self.deck, self.cardtypes, self.extra)
-            conn.request('PUT', '/sync/client/params')
-            conn.send(client_params)
-        except httplib.HTTPException, exception:
-            raise SyncError("Handshaking: " + exception)
+            sparams = urllib2.urlopen(self.uri + '/sync/server/params').read()
+            urllib2.urlopen(PutRequest(\
+                self.uri + '/sync/client/params', cparams))
+        except urllib2.URLError, error:
+            raise SyncError("Handshaking: " + str(error))
         else:
-            self.eman.set_sync_params(server_params)
-        finally:
-            conn.close()
+            self.eman.set_sync_params(sparams)
 
     def set_params(self, params):
         """Uses for setting non-default params."""
@@ -108,27 +98,15 @@ class Client:
         """Connects to server and gets server history."""
 
         try:
-            conn = httplib.HTTPConnection(self.host, self.port)
-            conn.request('GET', '/sync/server/history')
-            server_history = conn.getresponse().read()
-        except httplib.HTTPException, exception:
-            raise SyncError("Getting server history: " + exception)
-        else:
-            return server_history
-        finally:
-            conn.close()
+            return urllib2.urlopen(self.uri + '/sync/server/history').read()
+        except urllib2.URLError, error:
+            raise SyncError("Getting server history: " + str(error))
         
-    def send_history(self, history):
+    def send_client_history(self, history):
         """Sends client history to server."""
 
         try:
-            conn = httplib.HTTPConnection(self.host, self.port)
-            conn.request('PUT', '/sync/client/history')
-            conn.send(history)
-            response = conn.getresponse().read()
-        except httplib.HTTPException, exception:
-            raise SyncError("Sending client history: " + exception)
-        else:
-            return response
-        finally:
-            conn.close()
+            urllib2.urlopen(PutRequest(\
+                self.uri + '/sync/client/history', history))
+        except urllib2.URLError, error:
+            raise SyncError("Sending client history: " + str(error))
