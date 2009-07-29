65ffd076a1a7488b0d52aa381fdafd4422bc0440
umax <maxusachev@gmail.com>
2009-07-29 10:34:32 +0300

    returned to previous XML format

--- mnemosyne/libSM2sync/sync.py
+++ mnemosyne/libSM2sync/sync.py
@@ -8,7 +8,8 @@
 
 from mnemosyne.libmnemosyne.tag import Tag
 from mnemosyne.libmnemosyne.fact import Fact
-from mnemosyne.libmnemosyne.databases.SQLite_logging import SQLiteLogging as events
+from mnemosyne.libmnemosyne.databases.SQLite_logging \
+    import SQLiteLogging as events
 from xml.etree import ElementTree
 
 PROTOCOL_VERSION = 0.1
@@ -18,6 +19,7 @@ N_SIDED_CARD_TYPE = 3
 
 
 class SyncError(Exception):
+    """Sync exception class."""
     pass
 
 
@@ -78,23 +80,24 @@ class EventManager:
 
     def create_tag_xml_element(self, event):
         tag = self.database.get_tag(event['id'], False)
-        return "<tag ev='%s' id='%s' name='%s'/>" % \
+        return "<i><t>tag</t><ev>%s</ev><id>%s</id><name>%s</name></i>" % \
             (event['event'], tag.id, tag.name)
 
     def create_fact_xml_element(self, event):
         fact = self.database.get_fact(event['id'], False)
         dkeys = ','.join(["%s" % key for key, val in fact.data.items()])
-        dvalues = ' '.join(["dv%s='%s'" % (num, fact.data.values()[num]) \
-            for num in range(len(fact.data))])
-        return "<fact ev='%s' id='%s' ctid='%s' dk='%s' %s tm='%s'/>" % \
-            (event['event'], fact.id, fact.card_type.id, dkeys, dvalues, \
-            event['time'])
+        dvalues = ''.join(["<dv%s>%s</dv%s>" % (num, fact.data.values()[num], \
+        num) for num in range(len(fact.data))])
+        return "<i><t>fact</t><ev>%s</ev><id>%s</id><ctid>%s</ctid><dk>%s</dk>"\
+            "%s<tm>%s</tm></i>" % (event['event'], fact.id, fact.card_type.id, \
+            dkeys, dvalues, event['time'])
 
     def create_card_xml_element(self, event):
         card = self.database.get_card(event['id'], False)
-        return "<card ev='%s' id='%s' ctid='%s' fid='%s' fvid='%s'" \
-            " tags='%s' gr='%s' e='%s' lr='%s' nr='%s' sint='%s' aint='%s'" \
-            " nint='%s' ttm='%s' tm='%s'/>" % (event['event'], card.id, \
+        return "<i><t>card</t><ev>%s</ev><id>%s</id><ctid>%s</ctid><fid>%s" \
+            "</fid><fvid>%s</fvid><tags>%s</tags><gr>%s</gr><e>%s</e><lr>%s" \
+            "</lr><nr>%s</nr><si>%s</si><ai>%s</ai><ni>%s</ni><ttm>%s</ttm>" \
+            "<tm>%s</tm></i>" % (event['event'], card.id, \
             card.fact.card_type.id, card.fact.id, card.fact_view.id, \
             ','.join([item.name for item in card.tags]), card.grade, \
             card.easiness, card.last_rep, card.next_rep, event['s_int'], \
@@ -103,76 +106,78 @@ class EventManager:
     def create_card_type_xml_element(self, event):
         cardtype = self.database.get_card_type(event['id'], False)
         fields = [key for key, value in cardtype.fields]
-        return "<ctype ev='%s' id='%s' name='%s' f='%s' uf='%s' ks='%s'" \
-            " edata='%s'/>" % (event['event'], cardtype.id, cardtype.name, \
-            ','.join(fields), ','.join(cardtype.unique_fields), '', '')
+        return "<i><t>ctype</t><ev>%s</ev><id>%s</id><name>%s</name><f>%s</f>"\
+        "<uf>%s</uf><ks>%s</ks><edata>%s</edata></i>" % (event['event'], \
+        cardtype.id, cardtype.name, ','.join(fields), \
+        ','.join(cardtype.unique_fields), '', '')
 
     def create_object_from_xml(self, item):
         class DictClass(dict):
             pass
-        obj_type = item.tag
+        obj_type = item.find('t').text
         if obj_type == 'fact':
-            dkeys = item.get('dk').split(',')
-            dvals = [item.get("dv%s" % num) for num in range(len(dkeys))]
+            dkeys = item.find('dk').text.split(',')
+            dvals = [item.find("dv%s" % num).text for num in range(len(dkeys))]
             fact_data = dict([(key, dvals[dkeys.index(key)]) for key in dkeys])
-            card_type = self.database.get_card_type(item.get('ctid'), False)
-            creation_time = int(item.get('tm'))
-            fact_id = item.get('id')
+            card_type = self.database.get_card_type(\
+                item.find('ctid').text, False)
+            creation_time = int(item.find('tm').text)
+            fact_id = item.find('id').text
             return Fact(fact_data, card_type, creation_time, fact_id)
         elif obj_type == 'card':
             card = DictClass()
-            card.id = item.get('id')
+            card.id = item.find('id').text
             card.fact_view = DictClass()
-            card.fact_view.id = item.get('fvid')
-            card.fact = self.database.get_fact(item.get('fid'), False)
+            card.fact_view.id = item.find('fvid').text
+            card.fact = self.database.get_fact(item.find('fid').text, False)
             card.tags = set(self.database.get_or_create_tag_with_name(\
-                tag_name) for tag_name in item.get('tags').split(','))
-            card.grade = int(item.get('gr'))
-            card.easiness = float(item.get('e'))
+                tag_name) for tag_name in item.find('tags').text.split(','))
+            card.grade = int(item.find('gr').text)
+            card.easiness = float(item.find('e').text)
             card.acq_reps, card.ret_reps = 0, 0
             card.lapses = 0
             card.acq_reps_since_lapse, card.ret_reps_since_lapse = 0, 0
-            card.last_rep = int(item.get('lr'))
-            card.next_rep = int(item.get('nr'))
+            card.last_rep = int(item.find('lr').text)
+            card.next_rep = int(item.find('nr').text)
             card.extra_data, card.scheduler_data = 0, 0
             card.active, card.in_view = True, True
             #for repetition event
             try:
-                card.scheduled_interval = int(item.get('sint'))
+                card.scheduled_interval = int(item.find('si').text)
             except ValueError:
                 card.scheduled_interval = ''
             try:
-                card.actual_interval = int(item.get('aint'))
+                card.actual_interval = int(item.find('ai').text)
             except ValueError:
                 card.actual_interval = ''
             try:
-                card.new_interval = int(item.get('nint'))
+                card.new_interval = int(item.find('ni').text)
             except:
                 card.new_interval = ''
             try:
-                card.thinking_time = int(item.get('ttm'))
+                card.thinking_time = int(item.find('ttm').text)
             except:
                 card.thinking_time = ''
             return card
         elif obj_type == 'tag':
-            return Tag(item.get('name'), item.get('id'))
+            return Tag(item.find('name').text, item.find('id').text)
         elif obj_type == 'ctype':
             cardtype = DictClass()
-            cardtype.id = item.get('id')
-            cardtype.name = item.get('name')
-            cardtype.fields = item.get('f').split(',')
-            cardtype.unique_fields = item.get('uf').split(',')
+            cardtype.id = item.find('id').text
+            cardtype.name = item.find('name').text
+            cardtype.fields = item.find('f').text.split(',')
+            cardtype.unique_fields = item.find('uf').text.split(',')
             cardtype.keyboard_shortcuts, cardtype.extra_data = {}, {}
             return cardtype
 
     def apply_history(self, history):
         """Parses XML history and apply it to database."""
 
-        for child in ElementTree.fromstring(history).getchildren():
-            event = int(child.get('ev'))
+        for child in ElementTree.fromstring(history).findall('i'):
+            event = int(child.find('ev').text)
             obj = self.create_object_from_xml(child)
             if event == events.ADDED_FACT:
-                if not self.database.has_fact_with_data(obj):
+                if not self.database.duplicates_for_fact(obj):
                     self.database.add_fact(obj)
             elif event == events.UPDATED_FACT:
                 self.database.update_fact(obj)
@@ -186,7 +191,7 @@ class EventManager:
             elif event == events.DELETED_TAG:
                 self.database.delete_tag(obj)
             elif event == events.ADDED_CARD:
-                if not has_card_with_external_id(obj.id):
+                if not self.database.has_card_with_external_id(obj.id):
                     self.database.add_card(obj)
             elif event == events.UPDATED_CARD:
                 self.database.update_card(obj)
