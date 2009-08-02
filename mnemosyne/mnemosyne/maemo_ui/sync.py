44534587cd46b18bd9f7cb86cc4a29e87747055f
umax <maxusachev@gmail.com>
2009-08-02 22:27:58 +0300

    final stage of sync UI development

--- mnemosyne/mnemosyne/maemo_ui/sync.py
+++ mnemosyne/mnemosyne/maemo_ui/sync.py
@@ -35,7 +35,8 @@ class SyncWidget(UiComponent):
         self.w_tree.signal_autoconnect(\
             dict([(sig, getattr(self, sig + "_cb")) for sig in \
                 ("sync_to_main_menu", "activate_client_mode", \
-                "activate_server_mode")]))
+                "activate_server_mode", "start_client_sync", \
+                "start_server_sync")]))
         self.conf = self.config()
         self.w_tree.get_widget(\
             "sync_mode_client_login_entry").set_text(self.conf['login'])
@@ -54,14 +55,46 @@ class SyncWidget(UiComponent):
     def activate_client_mode_cb(self, widget):
         """Switches to Client mode."""
 
-        self.w_tree.get_widget("sync_toolbar_server_mode_button").set_active(False)
+        self.w_tree.get_widget(\
+            "sync_toolbar_server_mode_button").set_active(False)
         self.w_tree.get_widget("sync_mode_role_switcher").set_current_page(1)
 
     def activate_server_mode_cb(self, widget):
         """Switches to Server mode."""
 
-        self.w_tree.get_widget("sync_toolbar_client_mode_button").set_active(False)
+        self.w_tree.get_widget(\
+            "sync_toolbar_client_mode_button").set_active(False)
         self.w_tree.get_widget("sync_mode_role_switcher").set_current_page(2)
+
+    def start_client_sync_cb(self, widget):
+        if not widget.get_active():
+            self.show_or_hide_containers(False, "client")
+            # start syncing
+        else:
+            # stop syncing
+            self.show_or_hide_containers(True, "client")
+
+    def start_server_sync_cb(self, widget):
+        if not widget.get_active():
+            self.show_or_hide_containers(False, "server")
+            # start syncing
+        else:
+            # stop syncing
+            self.show_or_hide_containers(True, "server")
+            
+    def show_or_hide_containers(self, show, name):
+        if show:
+            self.w_tree.get_widget("sync_mode_%s_params_table" % name).show()
+            self.w_tree.get_widget("sync_mode_%s_progress_label" % name).hide()
+        else:
+            self.w_tree.get_widget("sync_mode_%s_params_table" % name).hide()
+            self.w_tree.get_widget("sync_mode_%s_progress_label" % name).show()
+        self.w_tree.get_widget(\
+            "sync_toolbar_client_mode_button").set_sensitive(show)
+        self.w_tree.get_widget(\
+            "sync_toolbar_server_mode_button").set_sensitive(show)
+        self.w_tree.get_widget(\
+            "sync_toolbar_main_menu_button").set_sensitive(show)
 
     def sync_to_main_menu_cb(self, widget):
         """Returns to main menu."""
