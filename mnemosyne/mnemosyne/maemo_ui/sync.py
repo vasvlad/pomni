504834db2e06461be6efa1f9353c8a1ef0aa471c
umax <maxusachev@gmail.com>
2009-08-02 23:35:03 +0300

    added warning messages

--- mnemosyne/mnemosyne/maemo_ui/sync.py
+++ mnemosyne/mnemosyne/maemo_ui/sync.py
@@ -38,63 +38,85 @@ class SyncWidget(UiComponent):
                 "activate_server_mode", "start_client_sync", \
                 "start_server_sync")]))
         self.conf = self.config()
-        self.w_tree.get_widget(\
+        self.get_widget = self.w_tree.get_widget
+        self.get_widget(\
             "sync_mode_client_login_entry").set_text(self.conf['login'])
-        self.w_tree.get_widget(\
+        self.get_widget(\
             "sync_mode_client_passwd_entry").set_text(self.conf['user_passwd'])
 
     def activate(self):
         """Activate sync mode."""
 
-        self.w_tree.get_widget("sync_mode_role_switcher").set_current_page(0)
-        self.w_tree.get_widget(\
-            "sync_toolbar_client_mode_button").set_active(False)
-        self.w_tree.get_widget(\
-            "sync_toolbar_server_mode_button").set_active(False)
+        self.get_widget("sync_mode_role_switcher").set_current_page(0)
+        self.get_widget("sync_toolbar_client_mode_button").set_active(False)
+        self.get_widget("sync_toolbar_server_mode_button").set_active(False)
 
     def activate_client_mode_cb(self, widget):
         """Switches to Client mode."""
 
-        self.w_tree.get_widget(\
-            "sync_toolbar_server_mode_button").set_active(False)
-        self.w_tree.get_widget("sync_mode_role_switcher").set_current_page(1)
+        self.get_widget("sync_toolbar_server_mode_button").set_active(False)
+        self.get_widget("sync_mode_role_switcher").set_current_page(1)
 
     def activate_server_mode_cb(self, widget):
         """Switches to Server mode."""
 
-        self.w_tree.get_widget(\
-            "sync_toolbar_client_mode_button").set_active(False)
-        self.w_tree.get_widget("sync_mode_role_switcher").set_current_page(2)
+        self.get_widget("sync_toolbar_client_mode_button").set_active(False)
+        self.get_widget("sync_mode_role_switcher").set_current_page(2)
 
     def start_client_sync_cb(self, widget):
+        """Starts syncing as Client."""
+
+        client = None
         if not widget.get_active():
+            # user chosed "start sync"
             self.show_or_hide_containers(False, "client")
+            login = self.get_widget("sync_mode_client_login_entry").get_text()
+            passwd = self.get_widget("sync_mode_client_passwd_entry").get_text()
+            uri = self.get_widget("sync_mode_client_address_entry").get_text()
+            if not uri.startswith("http://"):
+                uri = "http://" + uri
+            # result = Client.start
+            result = True
             # start syncing
+            if not result:
+                self.main_widget().error_box("error")
+            else:
+                self.main_widget().information_box(\
+                    "Syncing was completed successfull")
+            self.show_or_hide_containers(True, "client")
         else:
             # stop syncing
             self.show_or_hide_containers(True, "client")
 
     def start_server_sync_cb(self, widget):
+        """Starts syncing as Server."""
+
         if not widget.get_active():
-            self.show_or_hide_containers(False, "server")
-            # start syncing
+            try:
+                port = int(self.get_widget(\
+                    "sync_mode_server_port_entry").get_text())
+            except ValueError:
+                self.main_widget().error_box("Wrong port number!")
+            else:
+                self.show_or_hide_containers(False, "server")
+                # start syncing
+                #self.show_or_hide_containers(True, "server")
         else:
             # stop syncing
             self.show_or_hide_containers(True, "server")
             
     def show_or_hide_containers(self, show, name):
+        """Manages containers."""
+
         if show:
-            self.w_tree.get_widget("sync_mode_%s_params_table" % name).show()
-            self.w_tree.get_widget("sync_mode_%s_progress_label" % name).hide()
+            self.get_widget("sync_mode_%s_params_table" % name).show()
+            self.get_widget("sync_mode_%s_progress_label" % name).hide()
         else:
-            self.w_tree.get_widget("sync_mode_%s_params_table" % name).hide()
-            self.w_tree.get_widget("sync_mode_%s_progress_label" % name).show()
-        self.w_tree.get_widget(\
-            "sync_toolbar_client_mode_button").set_sensitive(show)
-        self.w_tree.get_widget(\
-            "sync_toolbar_server_mode_button").set_sensitive(show)
-        self.w_tree.get_widget(\
-            "sync_toolbar_main_menu_button").set_sensitive(show)
+            self.get_widget("sync_mode_%s_params_table" % name).hide()
+            self.get_widget("sync_mode_%s_progress_label" % name).show()
+        self.get_widget("sync_toolbar_client_mode_button").set_sensitive(show)
+        self.get_widget("sync_toolbar_server_mode_button").set_sensitive(show)
+        self.get_widget("sync_toolbar_main_menu_button").set_sensitive(show)
 
     def sync_to_main_menu_cb(self, widget):
         """Returns to main menu."""
