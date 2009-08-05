3039026e66383514b0c3f8baa40eeeb0a1cb1708
umax <maxusachev@gmail.com>
2009-08-05 21:21:26 +0300

    removed unused code. pylinting

--- mnemosyne/mnemosyne/maemo_ui/tts.py
+++ mnemosyne/mnemosyne/maemo_ui/tts.py
@@ -1,10 +1,31 @@
+#!/usr/bin/python -tt
+# vim: sw=4 ts=4 expandtab ai
+#
+# Pomni. Learning tool based on spaced repetition technique
+#
+# Copyright (C) 2008 Pomni Development Team <pomni@googlegroups.com>
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License version 2 as published by the
+# Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+# 02110-1301 USA
+#
+
+"""
+TTS engine wrapper.
+"""
+
 import commands
 import os
-
-PITCH_MIN = 0
-PITCH_MAX = 100
-SPEED_MIN = 30
-SPEED_MAX = 200
 
 def is_available():
     """Checks espeak available on system."""
@@ -29,22 +50,24 @@ class TTS:
         except ImportError:
             import gst
             def play(fname):
+                """gst play function."""
                 player = gst.element_factory_make("playbin", "player")
                 player.set_property('uri', "file://%s" % fname)
                 player.set_state(gst.STATE_PLAYING)                
+
             self.play_sound = play
 
-    def set_pitch(self, pitch):
-        self.pitch = pitch
-
-    def set_speed(self, speed):
-        self.speed = speed
-
     def set_params(self, params):
+        """Sets current espeak params."""
+
         for param in params:
             setattr(self, param, params[param])
 
     def speak(self, text):
+        """Speak text."""
+
+        if not text:
+            return
         command = self.espeak
         command += " -v " + self.language + self.voice
         command += " -s " + str(self.speed)
