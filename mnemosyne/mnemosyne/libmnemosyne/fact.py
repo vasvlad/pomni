b7339cc9f983485d1526d5b9f74601aafd341dc4
Peter Bienstman <Peter.Bienstman@UGent.be>
2009-09-26 21:16:09 +0200

    Callbacks to activate cards after creation or edit.

--- mnemosyne/mnemosyne/libmnemosyne/fact.py
+++ mnemosyne/mnemosyne/libmnemosyne/fact.py
@@ -39,7 +39,7 @@ class Fact(CompareOnId):
     def __init__(self, data, card_type, creation_time=None, id=None):
         #print "Fact.py __init__ method"
         if creation_time is None:
-            creation_time = time.time()
+            creation_time = int(time.time())
         self.creation_time = creation_time
         self.modification_time = self.creation_time
         self.data = data
