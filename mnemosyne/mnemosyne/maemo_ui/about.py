71cd5c216ee51e39beaecdc79f8343f8ef71def6
umax <maxusachev@gmail.com>
2009-11-24 16:47:51 +0200

    redesigned AboutWidget UI creation

--- mnemosyne/mnemosyne/maemo_ui/about.py
+++ mnemosyne/mnemosyne/maemo_ui/about.py
@@ -25,7 +25,7 @@ Hildon UI. About Widget.
 """
 
 import os
-import gtk
+from mnemosyne.maemo_ui.widgets import create_about_ui
 from mnemosyne.libmnemosyne.ui_component import UiComponent
 
 
@@ -33,81 +33,12 @@ class AboutWidget(UiComponent):
     """About Widget."""
 
     def __init__(self, component_manager):
-        UiComponent.__init__(self, component_manager)
+        UiComponent.__init__(self, component_manager, )
         # create widgets
-        toplevel_table = gtk.Table(rows=1, columns=2)
-        toolbar_container = gtk.Notebook()
-        toolbar_container.set_show_tabs(False)
-        toolbar_container.set_size_request(82, 480)
-        toolbar_container.set_name('tags_mode_toolbar_container')
-        toolbar_table = gtk.Table(rows=5, columns=1, homogeneous=True)
-        menu_button = gtk.Button()
-        menu_button.set_size_request(80, 80)
-        menu_button.set_name('about_toolbar_main_menu_button')
+        self.page, menu_button = create_about_ui(self.main_widget().switcher, \
+            os.path.join(self.config()['theme_path'], "mnemosyne.png"))
+        # connect signals
         menu_button.connect('clicked', self.about_to_main_menu_cb)
-        info_container = gtk.Notebook()
-        info_container.set_show_border(False)
-        info_container.set_show_tabs(False)
-        info_box = gtk.VBox()
-        hbutton_box = gtk.HButtonBox()
-        text_box = gtk.HBox()
-        logo_box = gtk.HBox()
-        label_left = gtk.Label()
-        label_left.set_use_markup(True)
-        label_left.set_markup("<span foreground='white' size='small'><b>" \
-            "Developers:</b></span>\n<span foreground='white' size='small'>" \
-            "Max Usachev |</span> <span foreground='#299BFC' size='small'>" \
-            "maxusachev@gmail.com</span>\n<span foreground='white' size=" \
-            "'small'>Ed Bartosh |</span> <span foreground='#299BFC' size=" \
-            "'small'>bartosh@gmail.com</span>\n<span foreground='white' " \
-            "size='small'>Vlad Vasiliev |</span> <span foreground='#299BFC' " \
-            "size='small'>vlad@gas.by</span>\n\n<span foreground='white' " \
-            "size='small'><b>Designer:</b>\n</span><span foreground='white' " \
-            "size='small'>Andrew Zhilin |</span> <span foreground='#299BFC' " \
-            "size='small'>drew.zhilin@gmail.com</span>\n\n<span foreground=" \
-            "'white' size='small'><b>Development team:</b></span>\n<span " \
-            "foreground='#299BFC' size='small'>pomni@googlegroups.com</span>")
-        label_right = gtk.Label()
-        label_right.set_use_markup(True)
-        label_right.set_markup("<span foreground='white' size='small'><b>" \
-            "Special Thanks To:</b></span>\n<span foreground='white' size=" \
-            "'small'>Peter Bienstman</span>\n<span foreground='#299BFC' size=" \
-            "'small'>Peter.Bienstman@ugent.be</span>\n<span foreground=" \
-            "'#299BFC' size='small'>http://www.mnemosyne-proj.org/</span>" \
-            "\n<span size='x-large'></span><span foreground='white' size=" \
-            "'small'>\nGSoC 2009</span>\n<span foreground='#299BFC' size='"\
-            "small'>http://socghop.appspot.com/</span>\n<span size='x-large'>" \
-            "</span><span foreground='white' size='small'>\nMaemo community" \
-            "</span>\n<span foreground='#299BFC' size='small'>" \
-            "http://maemo.org/</span>")
-        logo = gtk.Image()
-        logo.set_from_file(os.path.join(self.config()['theme_path'], \
-            "mnemosyne.png"))
-        program_label = gtk.Label()
-        program_label.set_justify(gtk.JUSTIFY_CENTER)
-        program_label.set_use_markup(True)
-        program_label.set_markup("<span foreground='white' size='large'><b>" \
-            "Mnemosyne for Maemo</b></span>\n<span foreground='white' size=" \
-            "'large'>version 2.0.0 beta5</span>")
-        # packing widgets
-        logo_box.pack_start(logo, expand=False, fill=False, padding=10)
-        logo_box.pack_end(program_label, expand=False, fill=False)
-        hbutton_box.pack_start(logo_box)
-        text_box.pack_start(label_left)
-        text_box.pack_end(label_right)
-        info_box.pack_start(hbutton_box, expand=False)
-        info_box.pack_end(text_box)
-        info_container.append_page(info_box)
-        toolbar_table.attach(menu_button, 0, 1, 4, 5, xoptions=gtk.EXPAND, \
-            yoptions=gtk.EXPAND)
-        toolbar_container.append_page(toolbar_table)
-        toplevel_table.attach(toolbar_container, 0, 1, 0, 1, \
-            xoptions=gtk.SHRINK, yoptions=gtk.SHRINK|gtk.EXPAND|gtk.FILL)
-        toplevel_table.attach(info_container, 1, 2, 0, 1, \
-            xoptions=gtk.SHRINK|gtk.EXPAND|gtk.FILL, \
-            yoptions=gtk.SHRINK|gtk.EXPAND|gtk.FILL)
-        toplevel_table.show_all()
-        self.page = self.main_widget().switcher.append_page(toplevel_table)
 
     def activate(self):
         """Set necessary switcher page."""
--- mnemosyne/mnemosyne/maemo_ui/widgets.py
+++ mnemosyne/mnemosyne/maemo_ui/widgets.py
@@ -556,6 +556,82 @@ def create_configuration_ui(main_switcher):
         tts_voice_prev_button, tts_voice_next_button
 
 
+def create_about_ui(main_switcher, image_name):
+    """Creates AboutWidget UI."""
+
+    toplevel_table = gtk.Table(rows=1, columns=2)
+    toolbar_container = gtk.Notebook()
+    toolbar_container.set_show_tabs(False)
+    toolbar_container.set_size_request(82, 480)
+    toolbar_container.set_name('tags_mode_toolbar_container')
+    toolbar_table = gtk.Table(rows=5, columns=1, homogeneous=True)
+    menu_button = gtk.Button()
+    menu_button.set_size_request(80, 80)
+    menu_button.set_name('about_toolbar_main_menu_button')
+    info_container = gtk.Notebook()
+    info_container.set_show_border(False)
+    info_container.set_show_tabs(False)
+    info_box = gtk.VBox()
+    hbutton_box = gtk.HButtonBox()
+    text_box = gtk.HBox()
+    logo_box = gtk.HBox()
+    label_left = gtk.Label()
+    label_left.set_use_markup(True)
+    label_left.set_markup("<span foreground='white' size='small'><b>" \
+        "Developers:</b></span>\n<span foreground='white' size='small'>" \
+        "Max Usachev |</span> <span foreground='#299BFC' size='small'>" \
+        "maxusachev@gmail.com</span>\n<span foreground='white' size=" \
+        "'small'>Ed Bartosh |</span> <span foreground='#299BFC' size=" \
+        "'small'>bartosh@gmail.com</span>\n<span foreground='white' " \
+        "size='small'>Vlad Vasiliev |</span> <span foreground='#299BFC' " \
+        "size='small'>vlad@gas.by</span>\n\n<span foreground='white' " \
+        "size='small'><b>Designer:</b>\n</span><span foreground='white' " \
+        "size='small'>Andrew Zhilin |</span> <span foreground='#299BFC' " \
+        "size='small'>drew.zhilin@gmail.com</span>\n\n<span foreground=" \
+        "'white' size='small'><b>Development team:</b></span>\n<span " \
+        "foreground='#299BFC' size='small'>pomni@googlegroups.com</span>")
+    label_right = gtk.Label()
+    label_right.set_use_markup(True)
+    label_right.set_markup("<span foreground='white' size='small'><b>" \
+        "Special Thanks To:</b></span>\n<span foreground='white' size=" \
+        "'small'>Peter Bienstman</span>\n<span foreground='#299BFC' size=" \
+        "'small'>Peter.Bienstman@ugent.be</span>\n<span foreground=" \
+        "'#299BFC' size='small'>http://www.mnemosyne-proj.org/</span>" \
+        "\n<span size='x-large'></span><span foreground='white' size=" \
+        "'small'>\nGSoC 2009</span>\n<span foreground='#299BFC' size='"\
+        "small'>http://socghop.appspot.com/</span>\n\n<span size='x-large'>" \
+        "</span><span foreground='white' size='small'>\nMaemo community" \
+        "</span>\n<span foreground='#299BFC' size='small'>" \
+        "http://maemo.org/</span>")
+    logo = gtk.Image()
+    logo.set_from_file(image_name)
+    program_label = gtk.Label()
+    program_label.set_justify(gtk.JUSTIFY_CENTER)
+    program_label.set_use_markup(True)
+    program_label.set_markup("<span foreground='white' size='large'><b>" \
+        "Mnemosyne for Maemo</b></span>\n<span foreground='white' size=" \
+        "'large'>version 2.0.0 beta5</span>")
+    # packing widgets
+    logo_box.pack_start(logo, expand=False, fill=False, padding=10)
+    logo_box.pack_end(program_label, expand=False, fill=False)
+    hbutton_box.pack_start(logo_box)
+    text_box.pack_start(label_left)
+    text_box.pack_end(label_right)
+    info_box.pack_start(hbutton_box, expand=False)
+    info_box.pack_end(text_box)
+    info_container.append_page(info_box)
+    toolbar_table.attach(menu_button, 0, 1, 4, 5, xoptions=gtk.EXPAND, \
+        yoptions=gtk.EXPAND)
+    toolbar_container.append_page(toolbar_table)
+    toplevel_table.attach(toolbar_container, 0, 1, 0, 1, \
+        xoptions=gtk.SHRINK, yoptions=gtk.SHRINK|gtk.EXPAND|gtk.FILL)
+    toplevel_table.attach(info_container, 1, 2, 0, 1, \
+        xoptions=gtk.SHRINK|gtk.EXPAND|gtk.FILL, \
+        yoptions=gtk.SHRINK|gtk.EXPAND|gtk.FILL)
+    toplevel_table.show_all()
+    return main_switcher.append_page(toplevel_table), menu_button
+
+
 def create_media_dialog_ui():
     """Creates MediaDialog UI."""
 
