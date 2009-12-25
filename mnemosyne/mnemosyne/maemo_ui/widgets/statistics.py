f13e17928a7279bc188ed2cbe000ccc58c1f8fd8
vlad <vlad@toshiba2.oblgaz>
2009-12-25 23:29:38 +0200

    added new button - "S" Common statistics in Statistics Mode

--- mnemosyne/mnemosyne/maemo_ui/widgets/statistics.py
+++ mnemosyne/mnemosyne/maemo_ui/widgets/statistics.py
@@ -29,15 +29,30 @@ import gtk
 def create_statistics_ui(main_switcher, statistics_text):
     """Creates MaemoStatisticsWidget UI."""
 
+    def create_button(name, width=80, height=80):
+        button = gtk.Button()
+        button.set_size_request(width, height)
+        button.set_name(name)
+        return button
+
     toplevel_table = gtk.Table(rows=1, columns=2)
     toolbar_container = gtk.Notebook()
     toolbar_container.set_show_tabs(False)
     toolbar_container.set_size_request(82, 480)
     toolbar_container.set_name('one_button_container')
     toolbar_table = gtk.Table(rows=5, columns=1, homogeneous=True)
-    menu_button = gtk.Button()
-    menu_button.set_size_request(80, 80)
-    menu_button.set_name('main_menu_button')
+# create toolbar buttons
+    buttons = {}
+    buttons[0] = create_button('stat_toolbar_current_card_button')
+    buttons[1] = create_button('stat_toolbar_common_stat_button')
+    buttons[5] = create_button('main_menu_button')
+
+# packing toolbar buttons
+    for pos in buttons.keys():
+        toolbar_table.attach(buttons[pos], 0, 1, pos, pos + 1, \
+            xoptions=gtk.EXPAND, yoptions=gtk.EXPAND)
+    toolbar_container.add(toolbar_table)
+
     info_container = gtk.Notebook()
     info_container.set_show_border(False)
     info_container.set_show_tabs(False)
@@ -54,15 +69,12 @@ def create_statistics_ui(main_switcher, statistics_text):
     info_box.pack_start(label_title, expand=False, fill=True, padding=10)
     info_box.pack_start(label_text, expand=False, fill=True, padding=10)
     info_container.append_page(info_box)
-    toolbar_table.attach(menu_button, 0, 1, 4, 5, xoptions=gtk.EXPAND, \
-        yoptions=gtk.EXPAND)
-    toolbar_container.append_page(toolbar_table)
     toplevel_table.attach(toolbar_container, 0, 1, 0, 1, \
         xoptions=gtk.SHRINK, yoptions=gtk.SHRINK|gtk.EXPAND|gtk.FILL)
     toplevel_table.attach(info_container, 1, 2, 0, 1, \
         xoptions=gtk.SHRINK|gtk.EXPAND|gtk.FILL, \
         yoptions=gtk.SHRINK|gtk.EXPAND|gtk.FILL)
     toplevel_table.show_all()
-    return main_switcher.append_page(toplevel_table), menu_button
+    return main_switcher.append_page(toplevel_table), buttons[5] 
 
 
