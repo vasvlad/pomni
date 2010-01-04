36fefbcf050117200dad587caebdfc891b9a2998
umax <maxusachev@gmail.com>
2010-01-04 16:42:58 +0200

    fixed widgets layout in Tags mode

--- mnemosyne/mnemosyne/maemo_ui/widgets/tags.py
+++ mnemosyne/mnemosyne/maemo_ui/widgets/tags.py
@@ -21,13 +21,12 @@
 #
 
 """
-Hildon UI. Widgets for main.
+Hildon UI. Widgets for Tags mode.
 """
 
 import gtk
 from mnemosyne.maemo_ui.widgets.common import create_tag_checkbox
 
-import mnemosyne.maemo_ui.widgets.common as widgets
 
 def create_tags_ui(main_switcher):
     """Creates TagsWidget UI."""
@@ -39,26 +38,15 @@ def create_tags_ui(main_switcher):
         return button
 
     toplevel_table = gtk.Table(rows=1, columns=2, homogeneous=False)
+    toolbar_table = gtk.Table(rows=5, columns=1, homogeneous=True)
     # create toolbar container
     toolbar_container = gtk.Notebook()
     toolbar_container.set_show_tabs(False)
     toolbar_container.set_size_request(82, 480)
-    toolbar_container.set_name('review_toolbar_container')
- 
-    toolbar_table = gtk.Table(rows=5, columns=1, homogeneous=True)
-    
+    toolbar_container.set_name('two_button_container')
     # create toolbar buttons
-    buttons = {}
-    buttons[4] = create_button('stat_button')
-    buttons[5] = create_button('main_menu_button')
- 
-
-    # packing toolbar buttons
-    for pos in buttons.keys():
-        toolbar_table.attach(buttons[pos], 0, 1, pos, pos + 1, \
-            xoptions=gtk.EXPAND, yoptions=gtk.EXPAND)
-    toolbar_container.add(toolbar_table)
- 
+    statistics_button = create_button('stat_button')
+    menu_button = create_button('main_menu_button')
     # create tags frame
     tags_frame = gtk.Frame()
     tags_frame.set_name('html_container')
@@ -73,6 +61,11 @@ def create_tags_ui(main_switcher):
     tags_viewport.set_name('viewport_widget')
     tags_box = gtk.VBox()
     # packing widgets
+    toolbar_table.attach(statistics_button, 0, 1, 0, 1, \
+        xoptions=gtk.EXPAND, yoptions=gtk.EXPAND)
+    toolbar_table.attach(menu_button, 0, 1, 4, 5, \
+        xoptions=gtk.EXPAND, yoptions=gtk.EXPAND)
+    toolbar_container.add(toolbar_table)
     tags_viewport.add(tags_box)
     tags_scrolledwindow.add(tags_viewport)
     tags_eventbox.add(tags_scrolledwindow)
@@ -84,6 +77,6 @@ def create_tags_ui(main_switcher):
         yoptions=gtk.SHRINK|gtk.EXPAND|gtk.FILL, \
         xpadding=30, ypadding=30)
     toplevel_table.show_all()
-    return main_switcher.append_page(toplevel_table), tags_box, buttons[5], \
-           buttons[4]
+    return main_switcher.append_page(toplevel_table), tags_box, menu_button, \
+        statistics_button
 
