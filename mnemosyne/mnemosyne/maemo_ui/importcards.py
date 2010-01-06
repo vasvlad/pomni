9257ba220c841114c097a4fbe0f2e8498d179744
bartosh <bartosh@gmail.com>
2010-01-06 18:27:45 +0200

    first variant of import for anki format

--- mnemosyne/mnemosyne/maemo_ui/importcards.py
+++ mnemosyne/mnemosyne/maemo_ui/importcards.py
@@ -33,121 +33,16 @@ from mnemosyne.maemo_ui.widgets.importcards  import create_import_ui
 #    def __init__(self, component_manager, previous_mode=None):
 #        pass
 
-import time
-from mnemosyne.libmnemosyne.ui_components.dialogs import StatisticsDialog
-from mnemosyne.maemo_ui.widgets.statistics import create_statistics_ui
+from mnemosyne.maemo_ui.widgets.importcards.py import create_importcard_ui
 
 DAY = 24 * 60 * 60 # Seconds in a day.
 
 class ImportCardsWidget(StatisticsDialog):
-    """Statistics Widget."""
+    """Import Widget."""
 
-    def __init__(self, component_manager, previous_mode=None):
-        self.current_card_text = ""
-        self.common_text = ""
-        self.total_text = ""
-        self.tags_text = {}
-        StatisticsDialog.__init__(self, component_manager)
-        self.prepare_statistics()
-        # create widgets
-        self.page, self.mode_statistics_switcher, menu_button, \
-            current_card_button, common_button, \
-            tags_button = create_statistics_ui(\
-            self.main_widget().switcher, self.current_card_text, \
-            self.common_text, self.total_text, self.tags_text)
-        # connect signals
-        if previous_mode == 'Menu':
-            menu_button.connect('clicked', self.back_to_main_menu_cb)
-        else:
-            menu_button.connect('clicked', self.back_to_previous_mode_cb)
-
-        current_card_button.connect('released', \
-            self.current_card_statistics_cb)
-        common_button.connect('released', \
-            self.common_statistics_cb)
-        tags_button.connect('released', \
-            self.tags_statistics_cb)
-
-        #Change current page
-        if "last_variant_for_statistics_page" in self.config():
-            number_of_page = self.config()["last_variant_for_statistics_page"] 
-        else:
-            number_of_page = 2
-        if number_of_page == 0:
-            current_card_button.set_active(True)
-            self.current_card_statistics_cb(None)
-        elif number_of_page == 2:
-            tags_button.set_active(True)
-            self.tags_statistics_cb(None) 
-        else:
-            common_button.set_active(True)
-            self.common_statistics_cb(None) 
- 
-
-    def prepare_statistics(self):
-        """Preparing statistics text"""
-
-        card = self.review_controller().card
-
-        #Current card text
-        self.current_card_text = """<span  foreground='white'\
-        size="x-large">"""
-        if not card:
-            self.current_card_text += "No current card."
-        elif card.grade == -1:
-            self.current_card_text += \
-                "Unseen card, no statistics available yet."
-        else:
-            self.current_card_text += "Grade" + ": %d\n" % card.grade
-            self.current_card_text += "Easiness" + ": %1.2f\n" % card.easiness
-            self.current_card_text += "Repetitions" + ": %d\n" \
-                % (card.acq_reps + card.ret_reps)
-            self.current_card_text += "Lapses" + ": %d\n" % card.lapses
-            self.current_card_text += "Interval" + ": %d\n" \
-                % (card.interval / DAY)
-            self.current_card_text += "Last repetition" + ": %s\n" \
-                % time.strftime("%B %d, %Y", time.gmtime(card.last_rep))
-            self.current_card_text += "Next repetition" + ": %s\n" \
-                % time.strftime("%B %d, %Y", time.gmtime(card.next_rep))
-            self.current_card_text += \
-                "Average thinking time (secs)" + ": %d\n" \
-                % self.database().average_thinking_time(card)
-            self.current_card_text += "Total thinking time (secs)" + ": %d\n" \
-                % self.database().total_thinking_time(card)
-        self.current_card_text += "</span>"
-
-        #Common text
-        self.common_text = """<span  foreground='white'\
-        size="x-large">"""
-        grades = range(-1, 6)
-        self.common_text += "\n".join([ "Grade  %2i -  %i" % \
-                 (grade, self.database().card_count_for_grade \
-                            (grade)) for grade in grades])
-        self.common_text += "</span>"
-
-        self.total_text = """<span  foreground='white'\
-        size="x-large">"""
-        count_of_card = sum([ self.database().card_count_for_grade \
-                            (grade) for grade in grades])
-        self.total_text += "Cards - %i" % count_of_card 
-        self.total_text += "</span>"
-
-        #Tags text in dict
-        text_for_tag = ""
-        for _id, name in self.database().get_tags__id_and_name():
-            name = name.replace('<','')
-            name = name.replace('>','')
-            text_for_tag = """<span  foreground='white'\
-                    size="x-large">"""
-            text_for_tag += "\n".join([ "Grade %2i - %i" % \
-                (grade, self.database().card_count_for_grade_and__tag_id \
-                                        (grade, _id)) for grade in grades])
-            count_of_card = sum([ self.database(). \
-                  card_count_for_grade_and__tag_id \
-                  (grade, _id) for grade in grades])
-            text_for_tag += "\n\n<b>Total:      %i</b>" % count_of_card
-            text_for_tag += "</span>"
-            self.tags_text[name] = text_for_tag
+    def __init__(self, component_manager ):
+	self.page, menu_button, ok_button = create_importcard_ui( \
+	self.main_widget().switcher)	
 
     def activate(self):
         """Set necessary switcher page."""
@@ -164,23 +59,14 @@ class ImportCardsWidget(StatisticsDialog):
     def back_to_main_menu_cb(self, widget):
         """Returns to main menu."""
 
-        self.config()["last_variant_for_statistics_page"] = \
-            self.mode_statistics_switcher.get_current_page()
         self.main_widget().switcher.remove_page(self.page)
-        self.main_widget().menu_('statistics')
+        self.main_widget().menu_('importcard')
 
-    # callbacks
-    def current_card_statistics_cb(self, widget):
-        """Switches to the current card statistics page."""
+    def ok_button_cb(self, widget):
+        """Ok """
 
-        self.mode_statistics_switcher.set_current_page(0)
+    	from mnemosyne.libmnemosyne.file_formats.tsv import import_txt
 
-    def common_statistics_cb(self, widget):
-        """Switches to the current card statistics page."""
-
-        self.mode_statistics_switcher.set_current_page(1)
-        
-    def tags_statistics_cb(self, widget):
-        """Switches to the tags statistics page."""
-        self.mode_statistics_switcher.set_current_page(2)
-       
+        imported_cards = export_txt("./test/anki.txt", "test_anki") 	
+	print imported_cards
+      
